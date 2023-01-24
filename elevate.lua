b58a88cc6655806b566a4eeff252a9c10c13965f
Solarflame5 <47642904+Solarflame5@users.noreply.github.com>
2023-01-24 16:49:31 +0300

    peripheral wrapping implemented

--- elevate.lua
+++ elevate.lua
@@ -1,11 +1,26 @@
-local sensor = peripheral.wrap("manipulator_0")
-local piston = peripheral.wrap("redstone_integrator_0")
-ylevel = 0
-while true do
-    ylevel= sensor.sense()[1]["y"]
-    if math.floor(ylevel+0.5) == 0 then
-        piston.setOutput("up", true)
-        sleep(0.2)
-        piston.setOutput("up", false)
-    end
+local floorsetup = {
+    {"manipulator_0", "redstone_integrator_0"},
+    {"manipulator_1", "redstone_integrator_1"}
+}
+
+local floors = {}
+
+for i=1,#floorsetup do
+    floors[i] = {}
+    floors[i][1] = peripheral.wrap(floorsetup[i][1])
+    floors[i][2] = peripheral.wrap(floorsetup[i][2])
 end
+
+for i=1,#floors do
+    print(floors[i][1])
+    print(floors[i][2])
+end
+
+-- while true do
+--     ylevel= sensor.sense()[1]["y"]
+--     if math.floor(ylevel+0.5) == 0 then
+--         piston.setOutput("up", true)
+--         sleep(0.2)
+--         piston.setOutput("up", false)
+--     end
+-- end
