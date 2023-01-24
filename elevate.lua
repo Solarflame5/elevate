caa8fa02668b0fa301c4d776ac26c68d167c3510
Solarflame5 <47642904+Solarflame5@users.noreply.github.com>
2023-01-24 17:04:14 +0300

    Basic floor picker

--- elevate.lua
+++ elevate.lua
@@ -16,11 +16,20 @@ for i=1,#floors do
     print(floors[i][2])
 end
 
--- while true do
---     ylevel= sensor.sense()[1]["y"]
---     if math.floor(ylevel+0.5) == 0 then
---         piston.setOutput("up", true)
---         sleep(0.2)
---         piston.setOutput("up", false)
---     end
--- end
+local targetfloor
+local ylevel
+
+while true do
+    write("Enter destination floor: ")
+    targetfloor = io.read()
+    targetfloor = tonumber(targetfloor)
+    while true do
+        ylevel = floors[targetfloor][1].sense()[1]["y"]
+        if math.floor(ylevel+0.5) == 0 then
+        floors[targetfloor][2].setOutput("up", true)
+        sleep(0.2)
+        floors[targetfloor][2].setOutput("up", false)
+        break
+        end
+    end
+end
